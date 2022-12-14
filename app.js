640a39873a8951fd7966d65ecc16506f4306e21c
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2022-12-14 15:43:06 +0100

    feat: final

--- app.js
+++ app.js
@@ -10,15 +10,16 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-const date = new Date("December 4, 2022 01:15:00");
+// const date = new Date("December 4, 2022 01:15:00");
+const date = new Date();
 const day = date.getDay();
 const hour = date.getHours();
 
-// console.log(day, hour);
-if (day > 5 || hour < 9 || hour > 17) {
+console.log(day, hour);
+if (day < 1 || day > 5 || hour < 9 || hour > 17) {
   app.get("/*", (req, res, next) => {
-    res.render("/offHours", {
-      pageTitle: "OffHours",
+    res.render("offhours/offhours", {
+      pageTitle: "offHours",
     });
   });
 } else {
